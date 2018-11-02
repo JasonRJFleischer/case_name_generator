847254232bf8e62f59133ebc5fe4ff13c74d8915
JasonRJFleischer <jrjflei@gmail.com>
2018-11-02 06:46:39 -0400

    added generator.py to generate text based on a seed and the trained lstm nn

---
+++ generator.py
@@ -0,0 +1,21 @@
+# load the network weights
+filename = "weights-improvement-47-1.2219-bigger.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print("Seed:")
+print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print("\nDone.")
